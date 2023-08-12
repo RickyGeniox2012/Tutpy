155bd723b461bbfeb96ddd93937ec0b3243c9781
RickyGeniox2012 <genioluciani@gmail.com>
2023-08-12 13:22:53 +0200

    First Commit

---
+++ Tutpy01.py
@@ -0,0 +1,13 @@
+# Tutpy01
+# First tutorial. Using variables.
+
+name="Ricky"
+surname="Genio"
+print (name)
+# Output: Ricky
+
+print (name+" "+surname)
+#Output: Ricky Genio
+
+print ("Name and Surname: " + name + " " + surname)
+# Output: Name and Surname: Ricky Genio
